eda871c6146d242d696c8e41ff8f5395e3dc69b1
Cuican <1781434473@qq.com>
2020-11-19 17:49:18 +0800

    no message

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <div>瑶瑶</div>
+      <div>蒙桑</div>
     </div>
     <router-view/>
   </div>
